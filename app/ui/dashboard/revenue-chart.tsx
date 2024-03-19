c3d3fdb0e6a07b7499868065e0d59f878293a985
Evgeny I <zheniog@yandex.ru>
2024-03-19 23:35:10 +0100

    fetching data 1

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -17,23 +17,23 @@ export default async function RevenueChart({
   const chartHeight = 350;
   // NOTE: comment in this code when you get to this point in the course
 
-  // const { yAxisLabels, topLabel } = generateYAxis(revenue);
+  const { yAxisLabels, topLabel } = generateYAxis(revenue);
 
-  // if (!revenue || revenue.length === 0) {
-  //   return <p className="mt-4 text-gray-400">No data available.</p>;
-  // }
+  if (!revenue || revenue.length === 0) {
+    return <p className="mt-4 text-gray-400">No data available.</p>;
+  }
 
   return (
-    <div className="w-full md:col-span-4">
+    <div className="md:col-span-4 w-full">
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Recent Revenue
       </h2>
       {/* NOTE: comment in this code when you get to this point in the course */}
 
-      {/* <div className="rounded-xl bg-gray-50 p-4">
-        <div className="sm:grid-cols-13 mt-0 grid grid-cols-12 items-end gap-2 rounded-md bg-white p-4 md:gap-4">
+      <div className="bg-gray-50 p-4 rounded-xl">
+        <div className="items-end gap-2 md:gap-4 grid grid-cols-12 sm:grid-cols-13 bg-white mt-0 p-4 rounded-md">
           <div
-            className="mb-6 hidden flex-col justify-between text-sm text-gray-400 sm:flex"
+            className="sm:flex flex-col justify-between hidden mb-6 text-gray-400 text-sm"
             style={{ height: `${chartHeight}px` }}
           >
             {yAxisLabels.map((label) => (
@@ -44,22 +44,22 @@ export default async function RevenueChart({
           {revenue.map((month) => (
             <div key={month.month} className="flex flex-col items-center gap-2">
               <div
-                className="w-full rounded-md bg-blue-300"
+                className="bg-blue-300 rounded-md w-full"
                 style={{
                   height: `${(chartHeight / topLabel) * month.revenue}px`,
                 }}
               ></div>
-              <p className="-rotate-90 text-sm text-gray-400 sm:rotate-0">
+              <p className="text-gray-400 text-sm -rotate-90 sm:rotate-0">
                 {month.month}
               </p>
             </div>
           ))}
         </div>
-        <div className="flex items-center pb-2 pt-6">
-          <CalendarIcon className="h-5 w-5 text-gray-500" />
-          <h3 className="ml-2 text-sm text-gray-500 ">Last 12 months</h3>
+        <div className="flex items-center pt-6 pb-2">
+          <CalendarIcon className="w-5 h-5 text-gray-500" />
+          <h3 className="ml-2 text-gray-500 text-sm">Last 12 months</h3>
         </div>
-      </div> */}
+      </div>
     </div>
   );
 }
