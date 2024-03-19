2c2a7d147dd54156a57b30ff0fbdb430d6ea8cfb
“Evgeny <zheniog@yandex.ru>
2024-03-19 19:01:12 +0100

    styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,20 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import styles from '@/app/ui/home.module.css'
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+    
+    
+    
+    <main className="flex flex-col p-6 min-h-screen">
+      <div className="flex items-end bg-blue-500 p-4 rounded-lg h-20 md:h-52 shrink-0">
         {/* <AcmeLogo /> */}
       </div>
-      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+      <div className="flex md:flex-row flex-col gap-4 mt-4 grow">
+        <div className="flex flex-col justify-center gap-6 bg-gray-50 px-6 md:px-20 py-10 rounded-lg md:w-2/5">
+          {/* <div className={styles.shape} /> */}
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
@@ -19,12 +24,12 @@ export default function Page() {
           </p>
           <Link
             href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+            className="flex items-center gap-5 bg-blue-500 hover:bg-blue-400 px-6 py-3 rounded-lg font-medium text-sm text-white md:text-base transition-colors self-start"
           >
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+        <div className="flex justify-center items-center md:px-28 md:py-12 p-6 md:w-3/5">
           {/* Add Hero Images Here */}
         </div>
       </div>
